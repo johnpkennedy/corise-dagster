567d6def3ac9c8f98f39e66f63caeceb8799417e
John K <jpk@johnpkennedy.com>
2023-04-29 16:22:53 +0000

    WIP

--- week_4/workspaces/project/week_4.py
+++ week_4/workspaces/project/week_4.py
@@ -14,24 +14,49 @@ from dagster import (
 from workspaces.types import Aggregation, Stock
 
 
-@asset
-def get_s3_data():
-    pass
+@asset(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    description="Get a list of stocks from an S3 file.",
+    op_tags={"kind": "s3"},
+)
+def get_s3_data(context):
+    s3_key = context.op_config["s3_key"]
+    s3_data = context.resources.s3.get_data(s3_key)
+    stocks = list(Stock.from_list(r) for r in s3_data)
+    return stocks
 
 
-@asset
-def process_data():
-    pass
+@asset(
+    description="Given a list of stocks return an Aggregation with the highest value stock.",
+)
+def process_data(context, get_s3_data):
+    stocks = get_s3_data
+    highest_value_stock = max(stocks, key = lambda k: k.high)
+    result = Aggregation(date=highest_value_stock.date, high=highest_value_stock.high)
+    return result
 
 
-@asset
-def put_redis_data():
-    pass
+@asset(
+    description="Upload an Aggregation to Redis.",
+    required_resource_keys={"redis"},
+    op_tags={"kind": "redis"},       
+)
+def put_redis_data(context, process_data):
+    aggregation = process_data
+    context.resources.redis.put_data(str(aggregation.date), str(aggregation.high))
 
 
-@asset
-def put_s3_data():
-    pass
+@asset(
+    description="Upload an Aggregation to an S3 file.",
+    required_resource_keys={"s3"},
+    op_tags={"kind": "s3"},
+)
+def put_s3_data(context, process_data):
+    aggregation = process_data
+    d=datetime.utcnow().strftime('%Y-%m-%d')
+    key_name=f'/aggregation_{d}.csv'
+    context.resources.s3.put_data(key_name, aggregation)
 
 
 project_assets = load_assets_from_current_module()
